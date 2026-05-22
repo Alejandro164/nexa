81f2db0500c50a7b7d1190d5f7ac46bea502047b
Alejandro164 <achaves29cr@gmail.com>
2026-05-22 07:56:14 -0600

    mostrar componentes

--- src/main/java/com/empresa/proyecto/controller/ComponentesController.java
+++ src/main/java/com/empresa/proyecto/controller/ComponentesController.java
@@ -3,11 +3,16 @@ package com.empresa.proyecto.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class ComponentesController {
 
     @GetMapping("/componentes")
-    public String componentes() {
-        return "componentes";
+    public String componentes(HttpServletRequest request) {
+        if ("true".equals(request.getHeader("HX-Request"))) {
+            return "componentes/componentes :: htmx-content";
+        }
+        return "componentes/componentes";
     }
 }
