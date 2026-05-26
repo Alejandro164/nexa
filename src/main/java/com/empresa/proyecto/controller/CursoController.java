0b5e990526f63a1fc3130846aeed7e2d3a1ae244
Alejandro164 <achaves29cr@gmail.com>
2026-05-26 14:45:53 -0600

    mis curso

---
+++ src/main/java/com/empresa/proyecto/controller/CursoController.java
@@ -0,0 +1,19 @@
+package com.empresa.proyecto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class CursoController {
+
+    @GetMapping("/mis-cursos")
+    public String misCursos(Model model, HttpServletRequest request) {
+        if ("true".equals(request.getHeader("HX-Request"))) {
+            return "curso/mis-cursos :: htmx-content";
+        }
+        return "curso/mis-cursos";
+    }
+}
