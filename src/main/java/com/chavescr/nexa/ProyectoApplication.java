d74a71ee9be2f5f0c300c6ff1982ca99ebfd6fab
Alejandro164 <achaves29cr@gmail.com>
2026-09-01 19:52:34 -0600

    arreglo comit

--- src/main/java/com/chavescr/nexa/ProyectoApplication.java
+++ src/main/java/com/chavescr/nexa/ProyectoApplication.java
@@ -1,5 +1,6 @@
 package com.chavescr.nexa;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
@@ -8,7 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(ProyectoApplication.class, args);
+		SpringApplication.run(ProyectoApplication.class, args);
 	}
 
 }
