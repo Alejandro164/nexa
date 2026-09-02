6e516e9a654f446bf310bd21dfd336c9e6252e6c
Alejandro164 <achaves29cr@gmail.com>
2026-09-01 19:30:14 -0600

    add error

--- src/main/java/com/chavescr/nexa/ProyectoApplication.java
+++ src/main/java/com/chavescr/nexa/ProyectoApplication.java
@@ -1,6 +1,5 @@
 package com.chavescr.nexa;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
@@ -9,7 +8,7 @@ import org.springframework.cache.annotation.EnableCaching;
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoApplication.class, args);
+		// SpringApplication.run(ProyectoApplication.class, args);
 	}
 
 }
